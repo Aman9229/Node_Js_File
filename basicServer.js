a27d945ee012ac2d1cc79c79142d97b84726f3c5
Aman <Akp88714725@gmail.com>
2023-08-24 18:03:32 +0530

    Node Js Basics

---
+++ basicServer.js
@@ -0,0 +1,27 @@
+
+
+const http=require('http');
+http.createServer((req,resp)=>{
+  resp.write("<h1> hello aman !</h1>")
+  resp.end()
+  console.log("server is working")
+}).listen(4500)
+
+
+
+/*
+
+
+const http=require('http');
+http.createServer((req,resp)=>{
+  resp.writeHead(200,{"contact-type":"Application/json"});
+  resp.write(JSON.stringify({
+    name:'AMAN PATEL',
+    Email:'Akp88714725@gmail.com'
+  }))
+  resp.end()
+  console.log("server is working")
+}).listen(4500)
+
+
+*/
